edf0c799be073e0dc4d276aa7c1c89be3e9f28af
Pulkit Saxena <pulkit21aug@gmail.com>
2022-01-29 18:41:41 +0530

    SalesbyMatch

--- HackerRank/src/com/puls/hackerrank/SalesbyMatch.java
+++ HackerRank/src/com/puls/hackerrank/SalesbyMatch.java
@@ -7,7 +7,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class SockPair {
+public class SalesbyMatch {
 
     // Complete the sockMerchant function below.
     static int sockMerchant(int n, int[] ar) {
